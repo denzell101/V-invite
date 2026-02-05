3376b5041c3ceecb71a5e8870c3caa461cb12c6d
denshooo <mallaridenzelljoshua@gmail.com>
2026-02-06 00:30:04 +0800

    Create config.js
    for variables

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+
+title: "Will you be my valentine? ❤️",
+
+details:
+`Mind Museum Adventure 🧠
+Dinner at Arcovia 🍽️`,
+
+date: "Feb 14, 2026 13:00:00",
+
+mapLink:
+"https://www.google.com/maps",
+
+musicEmbed:
+"https://open.spotify.com/embed/track/1BxfuPKGuaTgP7aM0Bbdwr"
+
+};
