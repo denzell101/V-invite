58261b508823616b5fec644e138893b41532b161
denshooo <mallaridenzelljoshua@gmail.com>
2026-02-07 17:34:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,44 +1,27 @@
 const CONFIG = {
 
-title: "Will you be my Valentine? ❤️",
+title:"Will you be my Valentine? ❤️",
 
-message:
-"Hi Baby ❤️\n\nI know you wanted something like this, so I've made you one.\nPlease remember that in every Valentine's, I will always choose you for my date :)\n\nP.S. I love you so much baby!",
+message:"Hi Baby ❤️\n\nI know you wanted something like this, so I've made you one.\nPlease remember that in every Valentine's, I will always choose you for my date :)\n\nP.S. I love you so much baby!",
 
-details:
-`Mind Museum Adventure 🧠
+details:`Mind Museum Adventure 🧠
 Dinner at EMILIA — House of Filipino Food 🍽️`,
 
-date: "Feb 15, 2026 13:00:00",
+date:"Feb 15, 2026 13:00:00",
 
-musicEmbed:
-"https://open.spotify.com/embed/track/1fzAuUVbzlhZ1lJAx9PtY6",
+musicEmbed:"https://open.spotify.com/embed/track/1fzAuUVbzlhZ1lJAx9PtY6",
 
-photos:[
-"pic1.jpg",
-"pic2.jpg",
-"pic3.jpg"
-],
+photos:["pic1.jpg","pic2.jpg","pic3.jpg"],
 
 sky:{
-clouds:[
-"cloud1.jpg",
-"cloud2.jpg",
-"cloud3.jpg"
-],
+clouds:["cloud1.jpg","cloud2.jpg","cloud3.jpg"],
 sun:"sun.jpg",
 moon:"moon.jpg"
 },
 
 locations:[
-{
-name:"The Mind Museum",
-link:"https://maps.app.goo.gl/FSa3uiyXRKhucbM87"
-},
-{
-name:"Dinner at EMILIA 🍽️",
-link:"https://maps.app.goo.gl/kbNYwtScLEkM9GfM9"
-}
+{ name:"Route to Mind Museum 🧭", image:"route1.jpg", link:"https://maps.app.goo.gl/FSa3uiyXRKhucbM87"},
+{ name:"Route to Emilia 🍽️", image:"route2.jpg", link:"https://maps.app.goo.gl/kbNYwtScLEkM9GfM9"}
 ]
 
 };
